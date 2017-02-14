26636d04c9870040a4dcbd6c4c42712b10a6b3d5
Cheyenne Breglia <bregc134@newschool.edu>
2017-02-14 11:28:11 -0500

    gradient change

--- space/assets/js/app.js
+++ space/assets/js/app.js
@@ -2,15 +2,19 @@ console.log("hello");
 
 var foods = 0;
 var colors = [];
+var ingredients = [];
 
 $(".hit p").click(function() {
 	foods++;
 	if (foods < 4) {
 		colors.push($(this).css("color"));
+		ingredients.push($(this).html());
 		$(".num-foods").html(foods);
 	} 
 	if (foods == 3) {
 		$(".overlay").show();
+		$(".poem1").show();
+		$(".overlay").css("background","linear-gradient(to bottom, " + colors[0] + " 20%," + colors[1] + " 50%," + colors[2] + " 80%")
 	}
 	console.log("clicked");
 });
