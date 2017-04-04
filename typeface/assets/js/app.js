da2a63cac82782bbaa0be84339940afbd362b701
Cheyenne Breglia <bregc134@newschool.edu>
2017-04-04 11:02:26 -0400

    typeface update

--- typeface/assets/js/app.js
+++ typeface/assets/js/app.js
@@ -1,111 +1,137 @@
+var charsTyped = 0;
+
 $(window).keypress(function(e) {
-  console.log(e.which);
+  // console.log(e.which);
+  charsTyped++;
+  console.log(charsTyped);
+  if ((charsTyped*140 + 100) > $(window).width()) {
+    console.log("do new line stuff");
+    charsTyped = 0;
+    $(window).scrollTop($(window).scrollTop() + 200);
+  }
+  $(window).scrollTop({  
+  behavior: 'smooth' 
+});
+
+  e.preventDefault();
+
   if (e.which == 97 || e.which == 65) {
-  	$("body").append($(".a-marker").clone().removeClass("a-marker"));
+  	$(".stage").append($(".a-marker").clone().removeClass("a-marker"));
   }
 
   if (e.which == 98 || e.which == 66) {
-  	$("body").append($(".b-marker").clone().removeClass("b-marker"));
+  	$(".stage").append($(".b-marker").clone().removeClass("b-marker"));
   }
 
    if (e.which == 99 || e.which == 67) {
-  	$("body").append($(".c-marker").clone().removeClass("c-marker"));
+  	$(".stage").append($(".c-marker").clone().removeClass("c-marker"));
   }
 
    if (e.which == 100 || e.which == 68) {
-  	$("body").append($(".d-marker").clone().removeClass("d-marker"));
+  	$(".stage").append($(".d-marker").clone().removeClass("d-marker"));
   }
 
    if (e.which == 101 || e.which == 69) {
-  	$("body").append($(".e-marker").clone().removeClass("e-marker"));
+  	$(".stage").append($(".e-marker").clone().removeClass("e-marker"));
   }
 
    if (e.which == 102 || e.which == 70) {
-  	$("body").append($(".f-marker").clone().removeClass("f-marker"));
+  	$(".stage").append($(".f-marker").clone().removeClass("f-marker"));
   }
 
    if (e.which == 103 || e.which == 71) {
-  	$("body").append($(".g-marker").clone().removeClass("g-marker"));
+  	$(".stage").append($(".g-marker").clone().removeClass("g-marker"));
   }
 
    if (e.which == 104 || e.which == 72) {
-  	$("body").append($(".h-marker").clone().removeClass("h-marker"));
+  	$(".stage").append($(".h-marker").clone().removeClass("h-marker"));
   }
 
    if (e.which == 105 || e.which == 73) {
-  	$("body").append($(".i-marker").clone().removeClass("i-marker"));
+  	$(".stage").append($(".i-marker").clone().removeClass("i-marker"));
   }
 
    if (e.which == 106 || e.which == 74) {
-  	$("body").append($(".j-marker").clone().removeClass("j-marker"));
+  	$(".stage").append($(".j-marker").clone().removeClass("j-marker"));
   }
 
    if (e.which == 107 || e.which == 75) {
-  	$("body").append($(".k-marker").clone().removeClass("k-marker"));
+  	$(".stage").append($(".k-marker").clone().removeClass("k-marker"));
   }
 
    if (e.which == 108 || e.which == 76) {
-  	$("body").append($(".l-marker").clone().removeClass("l-marker"));
+  	$(".stage").append($(".l-marker").clone().removeClass("l-marker"));
   }
 
    if (e.which == 109 || e.which == 77) {
-  	$("body").append($(".m-marker").clone().removeClass("m-marker"));
+  	$(".stage").append($(".m-marker").clone().removeClass("m-marker"));
   }
 
    if (e.which == 110 || e.which == 78) {
-  	$("body").append($(".n-marker").clone().removeClass("n-marker"));
+  	$(".stage").append($(".n-marker").clone().removeClass("n-marker"));
   }
 
    if (e.which == 111 || e.which == 79) {
-  	$("body").append($(".o-marker").clone().removeClass("o-marker"));
+  	$(".stage").append($(".o-marker").clone().removeClass("o-marker"));
   }
 
    if (e.which == 112 || e.which == 80) {
-  	$("body").append($(".p-marker").clone().removeClass("p-marker"));
+  	$(".stage").append($(".p-marker").clone().removeClass("p-marker"));
   }
 
    if (e.which == 113 || e.which == 81) {
-  	$("body").append($(".q-marker").clone().removeClass("q-marker"));
+  	$(".stage").append($(".q-marker").clone().removeClass("q-marker"));
   }
 
    if (e.which == 114 || e.which == 82) {
-  	$("body").append($(".r-marker").clone().removeClass("r-marker"));
+  	$(".stage").append($(".r-marker").clone().removeClass("r-marker"));
   }
 
    if (e.which == 115 || e.which == 83) {
-  	$("body").append($(".s-marker").clone().removeClass("s-marker"));
+  	$(".stage").append($(".s-marker").clone().removeClass("s-marker"));
   }
 
    if (e.which == 116 || e.which == 84) {
-  	$("body").append($(".t-marker").clone().removeClass("t-marker"));
+  	$(".stage").append($(".t-marker").clone().removeClass("t-marker"));
   }
 
    if (e.which == 117 || e.which == 85) {
-  	$("body").append($(".u-marker").clone().removeClass("u-marker"));
+  	$(".stage").append($(".u-marker").clone().removeClass("u-marker"));
   }
 
    if (e.which == 118 || e.which == 86) {
-  	$("body").append($(".v-marker").clone().removeClass("v-marker"));
+  	$(".stage").append($(".v-marker").clone().removeClass("v-marker"));
   }
 
    if (e.which == 119 || e.which == 87) {
-  	$("body").append($(".w-marker").clone().removeClass("w-marker"));
+  	$(".stage").append($(".w-marker").clone().removeClass("w-marker"));
   }
 
    if (e.which == 120 || e.which == 88) {
-  	$("body").append($(".x-marker").clone().removeClass("x-marker"));
+  	$(".stage").append($(".x-marker").clone().removeClass("x-marker"));
   }
 
    if (e.which == 121 || e.which == 89) {
-  	$("body").append($(".y-marker").clone().removeClass("y-marker"));
+  	$(".stage").append($(".y-marker").clone().removeClass("y-marker"));
   }
 
    if (e.which == 122 || e.which == 90) {
-  	$("body").append($(".z-marker").clone().removeClass("z-marker"));
+  	$(".stage").append($(".z-marker").clone().removeClass("z-marker"));
   }
+
+   if (e.which == 32) {
+    $(".stage").append($(".space-marker").clone().removeClass("space-marker"));
+  }
+
+  //$( ".blinking-cursor" ).hide();
+$(window).width();
+
 
 
 });
+
+
+
 
 
 
