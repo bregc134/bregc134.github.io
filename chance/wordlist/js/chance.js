b7d29726cbe664992b1e78727dd0bc693f8f3639
Cheyenne Breglia <bregc134@newschool.edu>
2017-04-28 10:55:14 -0400

    chance poem

---
+++ chance/wordlist/js/chance.js
@@ -0,0 +1,140 @@
+var words = new Array("the");
+
+var a = new Array("a", "always", "able", "am", "about","America", "above", "among", "across", "amount", "act", "an", "action", "and", "actually", "angle", "add", "animal", "addition", "another", "adjective", "answer", "afraid", "any", "Africa", "anything", "after", "appear", "afternoon", "apple", "again", "are", "against", "area", "age", "arms", "ago", "army", "agreed", "around", "ahead", "arrived", "air", "art", "all", "as", "allow", "ask", "almost", "at", "alone", "away", "along", "already", "also", "although");
+
+var b = new Array("baby", "bird", "bill", "burning", "back", "bit", "business", "bad", "black", "but", "ball", "block", "buy", "bank", "blood", "by", "base", "blow", "be", "blue", "bear", "board", "beat", "boat", "beautiful", "body", "became", "bones", "because", "book", "become", "born", "bed", "both", "been", "bottom", "before", "bought", "began", "box", "begin", "boy", "behind", "branches", "being", "break", "believe", "bright", "bell", "bring", "belong", "British", "below", "broken", "beside", "brother", "best", "brought", "better", "brown",	"between", "build", "big", "building");
+
+var c = new Array("call", "came", "can", "cannot", "can’t", "capital", "captain", "car", "care", "carefully", "carry", "case", "cat", "catch", "cattle", "caught", "cause", "cells", "center", "cents", "century", "certain", "chance", "change", "chart", "check", "chief", "child", "children", "choose", "corn", "church", "corner", "circle", "correct", "city", "cost", "class", "cotton", "clean", "could", "clear", "couldn’t", "climbed", "count", "close", "country", "clothes", "course", "cloud", "covered", "coast", "cows", "cold",	"create", "color", "cried", "column", "crops", "come", "cross", "common", "crowd", "company", "current", "compare",	"cut", "complete", "compound", "conditions", "consider", "consonant", "contain", "continued", "control", "cook", "cool", "copy");
+
+var d = new Array("doctor", "dance", "does", "dark", "doesn’t", "day", "dog", "dead", "dollars", "deal", "done", "death", "don’t", "decided", "door", "decimal", "down", "deep", "draw", "describe", "drawing", "desert", "dress", "design", "drive", "details", "drop", "determine", "dry", "developed", "during", "dictionary", "did", "didn’t", "died", "difference", "different", "difficult", "direct", "direction", "discovered", "distance", "divided", "division", "do");
+
+var e = new Array("evening", "each", "ever", "early", "every", "ears", "everyone", "earth", "everything", "east", "exactly", "easy", "example", "eat", "except", "edge", "exciting", "effect", "exercise", "eggs", "expect", "eight", "experience", "either", "experiment", "electric", "explain", "elements", "express", "else", "eye", "end", "energy", "engine", "England", "English", "enjoy", "enough", "entered", "entire", "equal", "equation", "especially", "Europe", "even");
+
+var f = new Array("finished", "full", "face", "fire", "fun", "fact", "first", "factories", "fish", "factors", "fit", "fair", "five", "fall", "flat", "family", "floor", "famous", "flow", "far", "flowers", "farm", "fly", "farmers", "follow", "fast", "food", "father", "foot", "fear", "for", "feel", "force", "feeling", "forest", "feet", "farm", "fell", "forward", "felt", "found", "few", "four", "field", "fraction", "fig", "France", "fight", "free", "figure", "French", "filled", "fresh" "finally", "friends", "find", "from", "fine", "front", "fingers", "fruit");
+
+var g = new Array("game", "garden", "gas", "gave", "general", "get", "girl", "give", "glass", "go", "God", "gold", "gone", "good", "got", "government", "grass", "great", "Greek", "green", "grew", "ground", "group", "grow", "guess", "gun");
+
+var h = new Array("hole", "had", "home", "hair", "hope", "half", "horse", "hand", "hot", "happened", "hours", "happy", "house", "hard", "how", "has", "however", "hat", "huge", "have", "human", "he", "hundred", "head", "hunting", "hear", "heard", "heart", "heat", "heavy", "held", "help", "her", "here", "high", "hill", "him", "himself", "his", "history", "hit", "hold");
+
+var i = new Array("I", "ice", "idea", "if", "I’ll", "important", "in", "inches", "include", "increase", "Indian", "indicate", "industry", "information", "insects", "inside", "instead", "instruments", "interest", "interesting", "into", "iron", "is", "island", "isn’t", "it", "it’s", "itself");
+
+var j = new Array("just", "joined", "Japanese", "job", "jumped", "jump", "jacket", "jail", "joy", "jealous", "join", "judge", "juice", "judgment", "jewelry", "jet", "jelly", "jaw", "jar", "jam");
+
+var k = new Array("kept", "known", "keep", "key", "killed", "kind", "king", "knew", "know", "keen", "kill", "knife", "kick", "kid", "knit", "knock", "knot", "kiss", "kitchen", "kingdom", "knee", "kitchen", "kindness");
+
+var l = new Array("length", "located", "list", "lady", "listen", "lake", "little", "land", "live", "language", "large",	"long", "last", "look", "late", "lost", "laughed", "lot", "law", "loud", "lay",	"love", "lead",	"low", "learn",	"least", "leave", "led", "left", "legs", "less", "let",	"let’s", "letter", "level", "lie", "life", "lifted", "light", "like", "line");
+
+var m = new Array("machine", "made", "main", "major", "make", "man", "many", "map", "march", "mark", "match", "material", "matter", "may", "maybe", "me", "mean", "measure", "meat","meet", "melody", "members", "men", "metal", "method", "middle", "might", "mile", "milk", "million", "mind", "mine", "minutes", "miss", "modern", "molecules", "moment", "money", "months", "moon", "more", "morning", "most", "mother", "mountain", "mouth", "move", "movement", "much", "music", "must", "my");
+
+var n = new Array("name", "nation", "natural", "near", "necessary", "need", "never", "new", "next", "night", "no", "nor", "north", "northern", "nose", "not", "note", "nothing", "notice", "noun", "now", "number", "numeral");
+
+var o = new Array("office", "outside", "object", "observe", "ocean", "of", "off", "often", "oh", "oil", "old", "on", "once", "one", "only", "open", "opposite", "or", "order", "other", "our", "out", "over", "own", "oxygen");
+
+var p = new Array("printed", "park", "please", "page", "plural", "paint", "poem", "pair", "point", "paper",	"pole", "paragraph", "poor", "position", "part", "possible", "particular", "pounds", "party", "power", "passed", "practice", "past", "prepared", "pattern",	"present", "pay", "president", "people", "pretty", "perhaps", "probably", "period",	"problem", "person", "process", "phrase", "produce", "picked", "products", "picture", "property", "piece",	"provide", "place",	"pulled", "plains",	"pushed", "plan", "put", "plane", "planets", "plant", "play");
+
+var q = new Array("quiet", "quickly", "question", "quite", "qualify", "quality", "qualification", "queen", "quarter", "quiz", "quote", "quick", "quest", "quarterly");
+
+var r = new Array("race", "road", "radio", "rock", "rain", "rolled", "raised", "room", "ran", "root", "rather",	"rope", "reached",	"rose", "read",	"round", "ready", "row", "real","rule", "reason", "run", "received", "record", "red", "region",	"remain", "remember", "repeated",  "report", "represent", "rest", "result", "return", "rhythm", "rich", "ride", "right", "ring", "rise");
+
+var s = new Array("safe", "settled", "skin", "spread", "said", "seven", "sky", "spring", "sail", "several", "sleep", "square", "same", "shall",	"slowly", "stand", "sand", "shape",	"small", "stars", "sat", "sharp", "smell", "start", "save", "she", "smiled", "state", "saw", "ship", "snow", "statement", "say", "shoes", "so",	"stay", "scale", "shop", "soft", "steel");
+
+var t = new Array("table", "thought", "true", "tail", "thousands", "try", "take", "three", "tube", "talk",	"through",	"turn", "tall",	"thus",	"two", "teacher", "tied", "type", "team", "time", "tell", "tiny", "temperature", "to", "ten", "today");
+
+var u = new Array("underline", "understand", "uncle", "under", "unit", "until", "up", "upon", "us", "use", "usually", "ugly", "umbrella", "umpire", "undertake", "undo", "unusual", "uniform", "unhappy", "unite");
+
+var v = new Array("view", "value", "valley", "various", "verb", "very", "village", "visit", "voice", "vowel", "vacuum", "viking", "vacation", "valuable", "vast", "video", "victim", "vessel", "violent", "violet");
+
+var w = new Array("workers", "wrong", "wait", "which", "would", "walk",	"while", "wouldn’t", "wall", "white", "write", "want", "who", "written", "war", "whole", "wrong", "warm", "whose", "wrote", "was", "why", "wash", "wide", "Washington",	"wife",	"wasn’t", "wild", "watch",	"will",	"water", "win",	"waves", "wind");
+
+var x = new Array("xanthan", "xylophone", "xanthippe", "xi", "xanthocomic", "xenagogue", "xenagogy", "xenial", "xeniatrophobia", "xenium", "xenization", "xenocracy", "xenodochium", "xenodochy", "xenoglossy", "xenology", "xenomania", "xenomorph", "xenotansplantation", "xeric", "xeriscape", "xerochilia", "xerocopy", "xerography", "xesturgy", "xiphoid", "xoanon", "xtal", "xylographer");
+
+var y = new Array("yard", "year", "yellow", "yes", "yet", "you", "young", "your", "you’re", "yourself", "yawn", "yearly", "yell", "yesterday", "yet", "youth");
+
+var z = new Array("zebra", "zipper", "zero", "zone", "zoo", "zigzags", "zipping", "zinging", "zagging", "zoom", "zooming", "zootomy", "zipless", "zinger", "zoogeographers", "zest", "zestfulness", "zillionare", "zooplankton", "zookeepers", "zoometries", "zucchini", "zooning", "zodiac", "zip");
+
+// console.log(w);
+
+// for (var i=0; i< w.length; i++) {
+//   console.log(w[i] + " is " + w[i].length + " characters long");
+// }
+
+$("p").html(words[Math.floor(Math.random()*words.length)]);
+$("p").append(" ");
+$("p").append(a[Math.floor(Math.random()*a.length)]);
+$("p").append(" ");
+$("p").append(b[Math.floor(Math.random()*b.length)]);
+$("p").append(" ");
+$("p").append(c[Math.floor(Math.random()*c.length)]);
+$("p").append(" ");
+$("p").append(d[Math.floor(Math.random()*d.length)]);
+$("p").append(" ");
+$("p").append(e[Math.floor(Math.random()*e.length)]);
+$("p").append(" ");
+$("p").append(f[Math.floor(Math.random()*f.length)]);
+$("p").append(" ");
+$("p").append(g[Math.floor(Math.random()*g.length)]);
+$("p").append(" ");
+$("p").append(h[Math.floor(Math.random()*h.length)]);
+$("p").append(" ");
+$("p").append(i[Math.floor(Math.random()*i.length)]);
+$("p").append(" ");
+$("p").append(j[Math.floor(Math.random()*j.length)]);
+$("p").append(" ");
+$("p").append(k[Math.floor(Math.random()*k.length)]);
+$("p").append(" ");
+$("p").append(l[Math.floor(Math.random()*l.length)]);
+$("p").append(" ");
+$("p").append(m[Math.floor(Math.random()*m.length)]);
+$("p").append(" ");
+$("p").append(n[Math.floor(Math.random()*n.length)]);
+$("p").append(" ");
+$("p").append(o[Math.floor(Math.random()*o.length)]);
+$("p").append(" ");
+$("p").append(p[Math.floor(Math.random()*p.length)]);
+$("p").append(" ");
+$("p").append(q[Math.floor(Math.random()*q.length)]);
+$("p").append(" ");
+$("p").append(r[Math.floor(Math.random()*r.length)]);
+$("p").append(" ");
+$("p").append(s[Math.floor(Math.random()*s.length)]);
+$("p").append(" ");
+$("p").append(t[Math.floor(Math.random()*t.length)]);
+$("p").append(" ");
+$("p").append(u[Math.floor(Math.random()*u.length)]);
+$("p").append(" ");
+$("p").append(v[Math.floor(Math.random()*v.length)]);
+$("p").append(" ");
+$("p").append(w[Math.floor(Math.random()*w.length)]);
+$("p").append(" ");
+$("p").append(x[Math.floor(Math.random()*x.length)]);
+$("p").append(" ");
+$("p").append(y[Math.floor(Math.random()*y.length)]);
+$("p").append(" ");
+$("p").append(z[Math.floor(Math.random()*z.length)]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
